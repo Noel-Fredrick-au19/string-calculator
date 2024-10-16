e5d6da394ee33b661143dbc20bc0647b9c126bfa
noel.fredrick <noel.fredrick@cheersin.com>
2024-10-16 23:20:09 +0530

    feat: test case for handle different delimiters

--- tests/StringCalculator.test.ts
+++ tests/StringCalculator.test.ts
@@ -18,6 +18,10 @@ describe('StringCalculator', () => {
   it('should handle new lines between numbers', () => {
     expect(calculator.add('1\n2,3')).toBe(6);
   });
+
+  it('should handle different delimiters', () => {
+    expect(calculator.add('//;\n1;2')).toBe(3);
+  });
   
 
 })
